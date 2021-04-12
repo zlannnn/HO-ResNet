6c67029542eaae9e20ac14affd6d80d70d69d79a
zlannnn <15801823611@163.com>
2021-04-12 19:49:24 +0900

    Update HO-ResNet.py

--- HO-ResNet.py
+++ HO-ResNet.py
@@ -21,6 +21,10 @@ class LambdaLayer(nn.Module):
 Use Euler method, which is the stand ResNet
 1 block = 2layers para and flops
 """
+"""
+For shortcut option, I only tested A and identity.
+If you want use option B, I guess BN in B shall be removed.
+"""
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -88,11 +92,11 @@ class MidBlock(nn.Module):
                 )
 
     def forward(self, x):
-
+        shortcut = self.shortcut(x)
         out = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
-        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * out + self.shortcut(x)))))))
+        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * out + shortcut))))))
 
-        out += self.shortcut(x)
+        out += shortcut
         out = func.relu(out)
 
         return out
@@ -133,13 +137,13 @@ class ImprovedEuler(nn.Module):
                 )
 
     def forward(self, x):
-        outx = 0 * x
+        shortcut = self.shortcut(x)
         out = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
-        outx += 0.5 * out
-        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * out + self.shortcut(x)))))))
+        outx = 0.5 * out
+        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * out + shortcut))))))
         outx += 0.5 * out
 
-        outx += self.shortcut(x)
+        outx += self.shortcut
         out = func.relu(outx)
 
         return out
@@ -179,13 +183,13 @@ class RK2Block(nn.Module):
                 )
 
     def forward(self, x):
-        outx = 0 * x
+        shortcut = self.shortcut(x)
         out = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
-        outx += 0.25 * out
-        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.666666 * out + self.shortcut(x)))))))
+        outx = 0.25 * out
+        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.666666 * shortcut))))))
         outx += 0.75 * out
 
-        outx += self.shortcut(x)
+        outx += shortcut
         out = func.relu(outx)
 
         return out
@@ -230,14 +234,14 @@ class Heun3Block(nn.Module):
                 )
 
     def forward(self, x):
-        outx = 0 * x
+        shortcut = self.shortcut(x)
         out = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
-        outx += 0.25 * out
-        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.333333 * out + self.shortcut(x)))))))
-        out = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(0.666666 * out + self.shortcut(x)))))))
+        outx = 0.25 * out
+        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.333333 * out + shortcut))))))
+        out = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(0.666666 * out + shortcut))))))
         outx += 0.75 * out
 
-        outx += self.shortcut(x)
+        outx += shortcut
         out = func.relu(outx)
 
         return out
@@ -283,15 +287,15 @@ class RK3Block(nn.Module):
                 )
 
     def forward(self, x):
-        outx = 0 * x
+        shortcut = self.shortcut(x)
         k1 = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
-        outx += 1/6. * k1
-        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * k1 + self.shortcut(x)))))))
+        outx = 1/6. * k1
+        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * k1 + shortcut))))))
         outx += 2/3. * k2
-        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(2*k2-k1 + self.shortcut(x)))))))
+        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(2*k2-k1 + shortcut))))))
         outx += 1/6. * k3
 
-        outx += self.shortcut(x)
+        outx += shortcut
         out = func.relu(outx)
 
         return out
@@ -339,16 +343,16 @@ class RK4Block(nn.Module):
                 )
 
     def forward(self, x):
-
+        shortcut = self.shortcut(x)
         out = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
         outx = 1 / 6. * out
-        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * out + self.shortcut(x)))))))
+        out = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * out + shortcut))))))
         outx += 1 / 3. * out
-        out = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(0.5 * out + self.shortcut(x)))))))
+        out = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(0.5 * out + shortcut))))))
         outx += 1 / 3. * out
-        out = self.conv8(func.relu(self.bn8(self.conv7(func.relu(self.bn7(out + self.shortcut(x)))))))
-        outx = 1 / 6. * out
-        out = outx + self.shortcut(x)
+        out = self.conv8(func.relu(self.bn8(self.conv7(func.relu(self.bn7(out + shortcut))))))
+        outx += 1 / 6. * out
+        out = outx + shortcut
         out = func.relu(out)
 
         return out
@@ -396,16 +400,16 @@ class Gill4Block(nn.Module):
                 )
 
     def forward(self, x):
-
+        shortcut = self.shortcut(x)
         k1 = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
         outx = 1 / 6. * k1
-        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * k1 + self.shortcut(x)))))))
+        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(0.5 * k1 + shortcut))))))
         outx += 0.097631 * k2
-        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(0.2071 * k1 + 0.29289 * k2 + self.shortcut(x)))))))
+        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(0.2071 * k1 + 0.29289 * k2 + shortcut))))))
         outx += 0.569 * k3
-        out = self.conv8(func.relu(self.bn8(self.conv7(func.relu(self.bn7(1.7071 * k3 - 0.7071 * k2 + self.shortcut(x)))))))
+        out = self.conv8(func.relu(self.bn8(self.conv7(func.relu(self.bn7(1.7071 * k3 - 0.7071 * k2 + shortcut))))))
         outx = 1 / 6. * out
-        out = outx + self.shortcut(x)
+        out = outx + shortcut
         out = func.relu(out)
 
         return out
@@ -464,15 +468,15 @@ class KuttaNys56Block(nn.Module):
                 )
 
     def forward(self, x):
-
+        shortcut = self.shortcut(x)
         k1 = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
-        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(1/3. * k1 + self.shortcut(x)))))))
-        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(1/25. * (4 * k1 + 6 * k2) + self.shortcut(x)))))))
-        k4 = self.conv8(func.relu(self.bn8(self.conv7(func.relu(self.bn7(1/4. * (k1 - 12 * k2 + 15 * k3) + self.shortcut(x)))))))
-        k5 = self.conv10(func.relu(self.bn10(self.conv9(func.relu(self.bn9(1/81. * (6 * k1 + 90 * k2 + - 50 * k3 + 8 * k4) + self.shortcut(x)))))))
-        k6 = self.conv12(func.relu(self.bn12(self.conv11(func.relu(self.bn11(1/75. *(6 * k1 + 36 * k2 + 10 * k3 + 8 * k4) + self.shortcut(x)))))))
+        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(1/3. * k1 + shortcut))))))
+        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(1/25. * (4 * k1 + 6 * k2) + shortcut))))))
+        k4 = self.conv8(func.relu(self.bn8(self.conv7(func.relu(self.bn7(1/4. * (k1 - 12 * k2 + 15 * k3) + shortcut))))))
+        k5 = self.conv10(func.relu(self.bn10(self.conv9(func.relu(self.bn9(1/81. * (6 * k1 + 90 * k2 + - 50 * k3 + 8 * k4) + shortcut))))))
+        k6 = self.conv12(func.relu(self.bn12(self.conv11(func.relu(self.bn11(1/75. *(6 * k1 + 36 * k2 + 10 * k3 + 8 * k4) + shortcut))))))
         out = 1/192. * (23 * k1 + 125 * k2 - 81 * k5 + 125 * k6)
-        out = out + self.shortcut(x)
+        out = out + shortcut
         out = func.relu(out)
 
         return out
@@ -540,23 +544,23 @@ class Huta68Block(nn.Module):
                 )
 
     def forward(self, x):
-
+        shortcut = self.shortcut(x)
         k1 = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
-        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(1/9. * k1 + self.shortcut(x)))))))
-        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(1/24. * (k1 + 3 * k2) + self.shortcut(x)))))))
+        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(1/9. * k1 + shortcut))))))
+        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(1/24. * (k1 + 3 * k2) + shortcut))))))
         k4 = self.conv8(func.relu(self.bn8(self.conv7(func.relu(
-            self.bn7(1/6. * (k1 - 3 * k2 + 4 * k3) + self.shortcut(x)))))))
+            self.bn7(1/6. * (k1 - 3 * k2 + 4 * k3) + shortcut))))))
         k5 = self.conv10(func.relu(self.bn10(self.conv9(func.relu(
-            self.bn9(1/8. * (-5 * k1 + 27 * k2 - 24 * k3 + 6 * k4) + self.shortcut(x)))))))
+            self.bn9(1/8. * (-5 * k1 + 27 * k2 - 24 * k3 + 6 * k4) + shortcut))))))
         k6 = self.conv12(func.relu(self.bn12(self.conv11(func.relu(
-            self.bn11(1/9. * (221 * k1 - 981 * k2 + 867 * k3 - 102*k4 + k5) + self.shortcut(x)))))))
+            self.bn11(1/9. * (221 * k1 - 981 * k2 + 867 * k3 - 102*k4 + k5) + shortcut))))))
         k7 = self.conv14(func.relu(self.bn14(self.conv13(func.relu(
-            self.bn13(1/48. *(-183 * k1 + 678 * k2 - 472 * k3 -66 * k4 +80 * k5 + 3*k6) + self.shortcut(x)))))))
+            self.bn13(1/48. *(-183 * k1 + 678 * k2 - 472 * k3 -66 * k4 +80 * k5 + 3*k6) + shortcut))))))
         k8 = self.conv16(func.relu(self.bn16(self.conv15(func.relu(
-            self.bn15(1/82. * (716 * k1 - 2079 * k2 + 1002 * k3 + 834 * k4 -454 * k5 - 9*k6 + 72 * k7) + self.shortcut(x)))))))
+            self.bn15(1/82. * (716 * k1 - 2079 * k2 + 1002 * k3 + 834 * k4 -454 * k5 - 9*k6 + 72 * k7) + shortcut))))))
         out = 1/840.*(41*k1+216*k3 +24*k4+ 272*k5 + 27*k6+ 216*k7+41*k8)
         # 8
-        out = out + self.shortcut(x)
+        out = out + shortcut
         out = func.relu(out)
 
         return out
@@ -625,20 +629,20 @@ class RKFehlberg(nn.Module):
                 )
 
     def forward(self, x):
-
+        shortcut = self.shortcut(x)
         k1 = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
-        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(1/4. * k1 + self.shortcut(x)))))))
-        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(3/32. * (k1 + 3 * k2) + self.shortcut(x)))))))
+        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(1/4. * k1 + shortcut))))))
+        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(3/32. * (k1 + 3 * k2) + shortcut))))))
         k4 = self.conv8(func.relu(self.bn8(self.conv7(func.relu(
-            self.bn7((1932/2197*k1 - 7200/2197 * k2 + 7296/32 * k3) + self.shortcut(x)))))))
+            self.bn7((1932/2197*k1 - 7200/2197 * k2 + 7296/32 * k3) + shortcut))))))
         k5 = self.conv10(func.relu(self.bn10(self.conv9(func.relu(
-            self.bn9((439/216. * k1 - 8 * k2 + 3680/513 * k3 - 845/4104 * k4) + self.shortcut(x)))))))
+            self.bn9((439/216. * k1 - 8 * k2 + 3680/513 * k3 - 845/4104 * k4) + shortcut))))))
         k6 = self.conv12(func.relu(self.bn12(self.conv11(func.relu(
-            self.bn11((-8/27 * k1 - 2 * k2 + 3544/2565 * k3 + 1859/4194*k4 - 11/40 * k5) + self.shortcut(x)))))))
+            self.bn11((-8/27 * k1 - 2 * k2 + 3544/2565 * k3 + 1859/4194*k4 - 11/40 * k5) + shortcut))))))
         k7 = self.conv14(func.relu(self.bn14(self.conv13(func.relu(
-            self.bn13(1/48. *(-183 * k1 + 678 * k2 - 472 * k3 -66 * k4 +80 * k5 + 3*k6) + self.shortcut(x)))))))
+            self.bn13(1/48. *(-183 * k1 + 678 * k2 - 472 * k3 -66 * k4 +80 * k5 + 3*k6) + shortcut))))))
         k8 = self.conv16(func.relu(self.bn16(self.conv15(func.relu(
-            self.bn15(1/82. * (716 * k1 - 2079 * k2 + 1002 * k3 + 834 * k4 -454 * k5 - 9*k6 + 72 * k7) + self.shortcut(x)))))))
+            self.bn15(1/82. * (716 * k1 - 2079 * k2 + 1002 * k3 + 834 * k4 -454 * k5 - 9*k6 + 72 * k7) + shortcut))))))
         y1 = x + 25/216 * k1 + 1408/2565 * k3 + 2197/4104*k4 - 1/5*k5
         y2 = x + 1/360 * k1 - 128/4275 * k3 + 2197/75240*k4 + 1/50*k5 + 2/55 * k6
         y2 = y2.abs()
@@ -649,7 +653,7 @@ class RKFehlberg(nn.Module):
         out = self.h * (1 / 840. * (41 * k1 + 216 * k3 + 24 * k4 + 272 * k5 + 27 * k6 + 216 * k7 + 41 * k8))
 
         # 8
-        out = out + self.shortcut(x)
+        out = out + shortcut
         out = func.relu(out)
 
         return out
@@ -750,33 +754,33 @@ class Verner89Block(nn.Module):
                 )
 
     def forward(self, x):
-
+        shortcut = self.shortcut(x)
         k1 = self.conv2(func.relu(self.bn2(self.conv1(func.relu(self.bn1(x))))))
         outx = 103 / 1680. * k1
-        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(1/12. * k1 + self.shortcut(x)))))))
-        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(1/27. * (k1 + 2 * k2) + self.shortcut(x)))))))
-        k4 = self.conv8(func.relu(self.bn8(self.conv7(func.relu(self.bn7(1/24. * (k1 + 3 * k3) + self.shortcut(x)))))))
-        k5 = self.conv10(func.relu(self.bn10(self.conv9(func.relu(self.bn9(1/375. * (234.25203582132 * k1 - 899.27141518056 * k3 + 837.49386649824 * k4) + self.shortcut(x)))))))
-        k6 = self.conv12(func.relu(self.bn12(self.conv11(func.relu(self.bn11((0.053333333333 * k1 + 0.2739534538729544 * k4 + 0.24567579393091227 * k5) + self.shortcut(x)))))))
-        k7 = self.conv14(func.relu(self.bn14(self.conv13(func.relu(self.bn13((0.06162164740427197 * k1 + 0.1815318224097963 * k4 - 0.013477689611 * k5 + 0.007024903611899742 * k6) + self.shortcut(x)))))))
-        k8 = self.conv16(func.relu(self.bn16(self.conv15(func.relu(self.bn15(1/54. * (4*k1+ 13.550510257220001 * k6 + 18.44948974278* k7) + self.shortcut(x)))))))
+        k2 = self.conv4(func.relu(self.bn4(self.conv3(func.relu(self.bn3(1/12. * k1 + shortcut))))))
+        k3 = self.conv6(func.relu(self.bn6(self.conv5(func.relu(self.bn5(1/27. * (k1 + 2 * k2) + shortcut))))))
+        k4 = self.conv8(func.relu(self.bn8(self.conv7(func.relu(self.bn7(1/24. * (k1 + 3 * k3) + shortcut))))))
+        k5 = self.conv10(func.relu(self.bn10(self.conv9(func.relu(self.bn9(1/375. * (234.25203582132 * k1 - 899.27141518056 * k3 + 837.49386649824 * k4) + shortcut))))))
+        k6 = self.conv12(func.relu(self.bn12(self.conv11(func.relu(self.bn11((0.053333333333 * k1 + 0.2739534538729544 * k4 + 0.24567579393091227 * k5) + shortcut))))))
+        k7 = self.conv14(func.relu(self.bn14(self.conv13(func.relu(self.bn13((0.06162164740427197 * k1 + 0.1815318224097963 * k4 - 0.013477689611 * k5 + 0.007024903611899742 * k6) + shortcut))))))
+        k8 = self.conv16(func.relu(self.bn16(self.conv15(func.relu(self.bn15(1/54. * (4*k1+ 13.550510257220001 * k6 + 18.44948974278* k7) + shortcut))))))
         outx -= -27 / 140. * k8
         #8
-        k9 = self.conv18(func.relu(self.bn18(self.conv17(func.relu(self.bn17(1/512 * (38*k1 + 61.66173591606*k6 + 174.33826408394 * k7 - 18 * k8) + self.shortcut(x)))))))
+        k9 = self.conv18(func.relu(self.bn18(self.conv17(func.relu(self.bn17(1/512 * (38*k1 + 61.66173591606*k6 + 174.33826408394 * k7 - 18 * k8) + shortcut))))))
         outx += 76 / 105. * k9
-        k10 = self.conv20(func.relu(self.bn20(self.conv19(func.relu(self.bn19(11/144. * k1 + 0.30503531279770835 * k6 + 0.31070542794303235 * k7 - 1/16. * k8 -8/27. * k9 + self.shortcut(x)))))))
+        k10 = self.conv20(func.relu(self.bn20(self.conv19(func.relu(self.bn19(11/144. * k1 + 0.30503531279770835 * k6 + 0.31070542794303235 * k7 - 1/16. * k8 -8/27. * k9 + shortcut))))))
         outx -= 201 / 280. * k10
-        k11 = self.conv22(func.relu(self.bn22(self.conv21(func.relu(self.bn21(0.07112936653168327*k1 + 0.37852828889059764 * k7 - 0.01174633003514941 * k8 + 0.07272054197227078*k9 - 0.26063186735940236* k10 + self.shortcut(x)))))))
+        k11 = self.conv22(func.relu(self.bn22(self.conv21(func.relu(self.bn21(0.07112936653168327*k1 + 0.37852828889059764 * k7 - 0.01174633003514941 * k8 + 0.07272054197227078*k9 - 0.26063186735940236* k10 + shortcut))))))
         outx += 1024 / 1365. * k11
-        k12 = self.conv24(func.relu(self.bn24(self.conv23(func.relu(self.bn23(-8.141639713845233 * k1 -574.4363925621823 * k6 + 847.8814814814815 * k7 + 113.71920186905155 * k8 + 626.9414848959715* k9 + 605.7315968367965 * k10 -328.69135802469134 * k11 + self.shortcut(x)))))))
+        k12 = self.conv24(func.relu(self.bn24(self.conv23(func.relu(self.bn23(-8.141639713845233 * k1 -574.4363925621823 * k6 + 847.8814814814815 * k7 + 113.71920186905155 * k8 + 626.9414848959715* k9 + 605.7315968367965 * k10 -328.69135802469134 * k11 + shortcut))))))
         outx += 3 / 7280. * k12
 
-        k13 = self.conv26(func.relu(self.bn26(self.conv25(func.relu(self.bn25(0.0878037592818966 * k1+0.6933735017296832*k6-1.9030978898036277*k7 + 0.22886338868515282*k8 -0.6904282483623702*k9 -0.07691188807394458* k10 +2624/ 1053.*k11 +3/1664.*k12 + self.shortcut(x)))))))
+        k13 = self.conv26(func.relu(self.bn26(self.conv25(func.relu(self.bn25(0.0878037592818966 * k1+0.6933735017296832*k6-1.9030978898036277*k7 + 0.22886338868515282*k8 -0.6904282483623702*k9 -0.07691188807394458* k10 +2624/ 1053.*k11 +3/1664.*k12 + shortcut))))))
         outx += 12 / 35. * k13
-        k14 = self.conv28(func.relu(self.bn28(self.conv27(func.relu(self.bn27(-137/1296.*k1 + 5.5746781906054865*k6 + 7.485506994579699 * k7 - 299/48. * k8 + 184/81. * k9 -44/9.* k10-5120/1053.*k11 - 11/468.* k12 +16/9.* k13 + self.shortcut(x)))))))
+        k14 = self.conv28(func.relu(self.bn28(self.conv27(func.relu(self.bn27(-137/1296.*k1 + 5.5746781906054865*k6 + 7.485506994579699 * k7 - 299/48. * k8 + 184/81. * k9 -44/9.* k10-5120/1053.*k11 - 11/468.* k12 +16/9.* k13 + shortcut))))))
         outx += 9 / 280. * k14
 
-        out = outx + self.shortcut(x)
+        out = outx + shortcut
         out = func.relu(out)
 
         return out
